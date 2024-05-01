e5539f7e7360807961e1906afc18d1468b1bfb5e
JAYARAJ <jayarajcs2011@hotmail.com>
2024-05-02 01:35:53 +0530

    LogoUpdated

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png";
+import styles from "./Logo.module.css";
 
 export default function Logo() {
-  return <img src={LogoImage} alt="logo" width={67} />;
+  return (
+    <div className={styles.logoDiv}>
+      <img src={LogoImage} alt="logo" width={67} />
+    </div>
+  );
 }
